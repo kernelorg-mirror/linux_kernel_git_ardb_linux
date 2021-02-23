c97339841eb3b55ea8e5e050e5bb08e18fcf08b1
Ard Biesheuvel <ardb@kernel.org>
2021-02-23 15:51:21 +0100

    arm64: kvm: enable trapping of host VM controls
    In order to restrict EL1's choice of TTBR0_EL1 translation tables, start
    trapping VM system register writes.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -154,7 +154,7 @@ int __pkvm_prot_finalize(void)
 
 	params->vttbr = kvm_get_vttbr(mmu);
 	params->vtcr = host_kvm.arch.vtcr;
-	params->hcr_el2 |= HCR_VM;
+	params->hcr_el2 |= HCR_VM | HCR_TVM;
 	kvm_flush_dcache_to_poc(params, sizeof(*params));
 
 	write_sysreg(params->hcr_el2, hcr_el2);
