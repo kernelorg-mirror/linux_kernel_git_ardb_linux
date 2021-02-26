94092d3df6c5818ae5cfed7e6ceda14c0799161d
Ard Biesheuvel <ardb@kernel.org>
2021-02-26 09:33:52 +0100

    mm: HACK provide target address when migrating a PMD entry

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2027,7 +2027,8 @@ static void __split_huge_zero_page_pmd(struct vm_area_struct *vma,
 		pte_unmap(pte);
 	}
 	smp_wmb(); /* make pte visible before pmd */
-	pmd_populate(mm, pmd, pgtable);
+	//pmd_populate(mm, pmd, pgtable);
+	set_pmd_at(mm, haddr, pmd, __pmd(page_to_phys(pgtable) | PMD_TYPE_TABLE));
 }
 
 static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
@@ -2193,7 +2194,8 @@ static void __split_huge_pmd_locked(struct vm_area_struct *vma, pmd_t *pmd,
 	}
 
 	smp_wmb(); /* make pte visible before pmd */
-	pmd_populate(mm, pmd, pgtable);
+	//pmd_populate(mm, pmd, pgtable);
+	set_pmd_at(mm, haddr, pmd, __pmd(page_to_phys(pgtable) | PMD_TYPE_TABLE));
 
 	if (freeze) {
 		for (i = 0; i < HPAGE_PMD_NR; i++) {
