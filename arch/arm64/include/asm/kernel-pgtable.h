3d72f694b443d4d014c82faad9aa28c444cdfc97
Ard Biesheuvel <ardb@kernel.org>
2022-06-29 17:59:27 +0200

    arm64: head: allocate more pages for the kernel mapping
    In preparation for switching to an early kernel mapping routine that
    maps each segment according to its precise boundaries, and with the
    correct attributes, let's allocate some extra pages for page tables for
    the 4k page size configuration. This is necessary because the start and
    end of each segment may not be aligned to the block size, and so we'll
    need an extra page table at each segment boundary.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm64/include/asm/kernel-pgtable.h
+++ arch/arm64/include/asm/kernel-pgtable.h
@@ -85,7 +85,7 @@
 			+ EARLY_PGDS((vstart), (vend), add) 	/* each PGDIR needs a next level page table */	\
 			+ EARLY_PUDS((vstart), (vend), add)	/* each PUD needs a next level page table */	\
 			+ EARLY_PMDS((vstart), (vend), add))	/* each PMD needs a next level page table */
-#define INIT_DIR_SIZE (PAGE_SIZE * EARLY_PAGES(KIMAGE_VADDR, _end, EARLY_KASLR))
+#define INIT_DIR_SIZE (PAGE_SIZE * (EARLY_PAGES(KIMAGE_VADDR, _end, EARLY_KASLR) + EARLY_SEGMENT_EXTRA_PAGES))
 
 /* the initial ID map may need two extra pages if it needs to be extended */
 #if VA_BITS < 48
@@ -104,6 +104,15 @@
 #define SWAPPER_BLOCK_SHIFT	PAGE_SHIFT
 #define SWAPPER_BLOCK_SIZE	PAGE_SIZE
 #define SWAPPER_TABLE_SHIFT	PMD_SHIFT
+#endif
+
+/* The number of segments in the kernel image (text, rodata, inittext, initdata, data+bss) */
+#define KERNEL_SEGMENT_COUNT	5
+
+#if SWAPPER_BLOCK_SIZE > SEGMENT_ALIGN
+#define EARLY_SEGMENT_EXTRA_PAGES (KERNEL_SEGMENT_COUNT + 1)
+#else
+#define EARLY_SEGMENT_EXTRA_PAGES 0
 #endif
 
 /*
