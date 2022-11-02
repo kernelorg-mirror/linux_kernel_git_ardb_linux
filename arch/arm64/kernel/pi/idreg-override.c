f9186d8851305d5cca5d22bf3c01c1d7f03a7747
Ard Biesheuvel <ardb@kernel.org>
2022-11-02 15:11:35 +0100

    arm64: idreg-override: Create a pseudo feature for rodata=off
    Add rodata=off to the set of kernel command line options that is parsed
    early using the CPU feature override detection code, so we can easily
    refer to it when creating the kernel mapping.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm64/kernel/pi/idreg-override.c
+++ arch/arm64/kernel/pi/idreg-override.c
@@ -133,6 +133,7 @@ DEFINE_OVERRIDE(5, smfr0, "id_aa64smfr0", id_aa64smfr0_override,
 
 DEFINE_OVERRIDE(6, sw_features, "arm64_sw", arm64_sw_feature_override,
 		FIELD("nokaslr", ARM64_SW_FEATURE_OVERRIDE_NOKASLR),
+		FIELD("rodataoff", ARM64_SW_FEATURE_OVERRIDE_RODATA_OFF),
 		{});
 
 /*
@@ -164,6 +165,7 @@ static const struct {
 	  "id_aa64isar2.gpa3=0 id_aa64isar2.apa3=0"	   },
 	{ "arm64.nomte",		"id_aa64pfr1.mte=0" },
 	{ "nokaslr",			"arm64_sw.nokaslr=1" },
+	{ "rodata=off",			"arm64_sw.rodataoff=1" },
 };
 
 static int __init find_field(const char *cmdline, char *opt, int len,
