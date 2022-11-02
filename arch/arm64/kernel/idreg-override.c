7dcd0a7bc1821bb5087f3c62cda1485c8209d797
Ard Biesheuvel <ardb@kernel.org>
2022-11-02 11:11:16 +0100

    arm64: idreg-override: avoid strlen() to check for empty strings
    strlen() is a costly way to decide whether a string is empty, as in that
    case, the first character will be NUL so we can check for that directly.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm64/kernel/idreg-override.c
+++ arch/arm64/kernel/idreg-override.c
@@ -196,7 +196,7 @@ static void __init match_options(const char *cmdline)
 		const struct ftr_set_desc *reg = offset_to_ptr(&regs[i]);
 		int f;
 
-		for (f = 0; strlen(reg->fields[f].name); f++) {
+		for (f = 0; reg->fields[f].name[0] != '\0'; f++) {
 			u64 shift = reg->fields[f].shift;
 			u64 width = reg->fields[f].width ?: 4;
 			u64 mask = GENMASK_ULL(shift + width - 1, shift);
