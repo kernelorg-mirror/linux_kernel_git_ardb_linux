692a5aa23fd798087aff1f40ca93340a808f3029
Ard Biesheuvel <ardb@kernel.org>
2022-12-06 17:45:37 +0100

    ARM: vfp: Manipulate VFP state with softirqs disabled
    In preparation for permitting kernel mode NEON to be used from softirq
    context, make sure that we manipulate the VFP hardware state with
    softirq processing disabled. This is necessary because a softirq
    processed over the back of an IRQ taken while in the middle of saving or
    restoring the VFP state may return with the NEON state clobbered, and
    this should not occur while the VFP bookkeeping is in an inconsistent
    state temporarily.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -416,7 +416,7 @@ void VFP_bounce(u32 trigger, u32 fpexc, struct pt_regs *regs)
 	if (exceptions)
 		vfp_raise_exceptions(exceptions, trigger, orig_fpscr, regs);
  exit:
-	preempt_enable();
+	local_bh_enable();
 }
 
 static void vfp_enable(void *unused)
@@ -517,6 +517,8 @@ void vfp_sync_hwstate(struct thread_info *thread)
 {
 	unsigned int cpu = get_cpu();
 
+	local_bh_disable();
+
 	if (vfp_state_in_hw(cpu, thread)) {
 		u32 fpexc = fmrx(FPEXC);
 
@@ -528,6 +530,7 @@ void vfp_sync_hwstate(struct thread_info *thread)
 		fmxr(FPEXC, fpexc);
 	}
 
+	local_bh_enable();
 	put_cpu();
 }
 
@@ -723,7 +726,8 @@ void kernel_neon_begin(void)
 	 * mode NEON register contents never need to be preserved.
 	 */
 	BUG_ON(in_interrupt());
-	cpu = get_cpu();
+	local_bh_disable();
+	cpu = __smp_processor_id();
 
 	fpexc = fmrx(FPEXC) | FPEXC_EN;
 	fmxr(FPEXC, fpexc);
@@ -746,7 +750,7 @@ void kernel_neon_end(void)
 {
 	/* Disable the NEON/VFP unit. */
 	fmxr(FPEXC, fmrx(FPEXC) & ~FPEXC_EN);
-	put_cpu();
+	local_bh_enable();
 }
 EXPORT_SYMBOL(kernel_neon_end);
 
