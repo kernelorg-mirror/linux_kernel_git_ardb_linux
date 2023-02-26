cbf6dc26923fd6b0fecfddd19e32fd026321660e
Ard Biesheuvel <ardb@kernel.org>
2023-02-26 21:08:37 +0100

    arm64: ptdump: Deal with translation levels folded at runtime
    Currently, the ptdump code deals with folded PMD or PUD levels at build
    time, by omitting those levels when invoking note_page. IOW, note_page()
    is never invoked with level == 1 if P4Ds are folded in the build
    configuration.
    
    With the introduction of LPA2 support, we will defer some of these
    folding decisions to runtime, so let's take care of this by overriding
    the 'level' argument when this condition triggers.
    
    Substituting the PUD or PMD strings for "PGD" when the level in question
    is folded at build time is no longer necessary, and so the conditional
    expressions can be simplified. This also makes the indirection of the
    'name' field unnecessary, so change that into a char[] array, and make
    the whole thing __ro_after_init.
    
    Note that the mm_p?d_folded() functions currently ignore their mm
    pointer arguments, but let's wire them up correctly anyway.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm64/mm/ptdump.c
+++ arch/arm64/mm/ptdump.c
@@ -82,6 +82,7 @@ struct pg_state {
 	struct ptdump_state ptdump;
 	struct seq_file *seq;
 	const struct addr_marker *marker;
+	const struct mm_struct *mm;
 	unsigned long start_address;
 	int level;
 	u64 current_prot;
@@ -178,12 +179,12 @@ static const struct prot_bits pte_bits[] = {
 
 struct pg_level {
 	const struct prot_bits *bits;
-	const char *name;
-	size_t num;
+	char name[4];
+	int num;
 	u64 mask;
 };
 
-static struct pg_level pg_level[] = {
+static struct pg_level pg_level[] __ro_after_init = {
 	{ /* pgd */
 		.name	= "PGD",
 		.bits	= pte_bits,
@@ -193,11 +194,11 @@ static struct pg_level pg_level[] = {
 		.bits	= pte_bits,
 		.num	= ARRAY_SIZE(pte_bits),
 	}, { /* pud */
-		.name	= (CONFIG_PGTABLE_LEVELS > 3) ? "PUD" : "PGD",
+		.name	= "PUD",
 		.bits	= pte_bits,
 		.num	= ARRAY_SIZE(pte_bits),
 	}, { /* pmd */
-		.name	= (CONFIG_PGTABLE_LEVELS > 2) ? "PMD" : "PGD",
+		.name	= "PMD",
 		.bits	= pte_bits,
 		.num	= ARRAY_SIZE(pte_bits),
 	}, { /* pte */
@@ -261,6 +262,11 @@ static void note_page(struct ptdump_state *pt_st, unsigned long addr, int level,
 	static const char units[] = "KMGTPE";
 	u64 prot = 0;
 
+	/* check if the current level has been folded dynamically */
+	if ((level == 1 && mm_p4d_folded(st->mm)) ||
+	    (level == 2 && mm_pud_folded(st->mm)))
+		level = 0;
+
 	if (level >= 0)
 		prot = val & pg_level[level].mask;
 
@@ -322,6 +328,7 @@ void ptdump_walk(struct seq_file *s, struct ptdump_info *info)
 	st = (struct pg_state){
 		.seq = s,
 		.marker = info->markers,
+		.mm = info->mm,
 		.level = -1,
 		.ptdump = {
 			.note_page = note_page,
