b14f5d2480c109ce2f13051a4e84c4b0d83ddece
Ard Biesheuvel <ardb@kernel.org>
2023-07-02 11:57:34 +0200

    acpi/prmt: Use EFI runtime sandbox to invoke PRM handlers
    Instead of bypassing the kernel's adaptation layer for performing EFI
    runtime calls, wire up ACPI PRM handling into it. This means these calls
    can no longer occur concurrently with EFI runtime calls, and will be
    made from the EFI runtime workqueue. It also means any page faults
    occurring during PRM handling will be identified correctly as
    originating in firmware code.
    
    While at it, give the function pointer struct member a more descriptive
    name so it will stand out in diagnostic messages if any issues do occur.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- drivers/firmware/efi/runtime-wrappers.c
+++ drivers/firmware/efi/runtime-wrappers.c
@@ -108,6 +108,13 @@ union efi_rts_args {
 		u64		*max_size;
 		int		*reset_type;
 	} QUERY_CAPSULE_CAPS;
+
+	struct {
+		efi_status_t (__efiapi	*acpi_prm_handler)(efi_physical_addr_t,
+							   void *);
+		efi_physical_addr_t	param_buffer_addr;
+		void			*context;
+	} ACPI_PRM_HANDLER;
 };
 
 struct efi_runtime_work efi_rts_work;
@@ -278,6 +285,14 @@ static void efi_call_rts(struct work_struct *work)
 				       args->QUERY_CAPSULE_CAPS.max_size,
 				       args->QUERY_CAPSULE_CAPS.reset_type);
 		break;
+	case EFI_ACPI_PRM_HANDLER:
+#ifdef CONFIG_ACPI_PRMT
+		status = efi_call_virt_pointer(&args->ACPI_PRM_HANDLER,
+					       acpi_prm_handler,
+					       args->ACPI_PRM_HANDLER.param_buffer_addr,
+					       args->ACPI_PRM_HANDLER.context);
+		break;
+#endif
 	default:
 		/*
 		 * Ideally, we should never reach here because a caller of this
@@ -544,3 +559,23 @@ void __init efi_native_runtime_setup(void)
 	efi.update_capsule = virt_efi_update_capsule;
 	efi.query_capsule_caps = virt_efi_query_capsule_caps;
 }
+
+#ifdef CONFIG_ACPI_PRMT
+
+efi_status_t
+efi_call_acpi_prm_handler(efi_status_t (__efiapi *handler_addr)(efi_physical_addr_t,
+							        void *),
+			  efi_physical_addr_t param_buffer_addr,
+			  void *context)
+{
+	efi_status_t status;
+
+	if (down_interruptible(&efi_runtime_lock))
+		return EFI_ABORTED;
+	status = efi_queue_work(ACPI_PRM_HANDLER, handler_addr,
+				param_buffer_addr, context);
+	up(&efi_runtime_lock);
+	return status;
+}
+
+#endif
--- include/linux/efi.h
+++ include/linux/efi.h
@@ -1231,6 +1231,12 @@ extern int efi_tpm_final_log_size;
 
 extern unsigned long rci2_table_phys;
 
+efi_status_t
+efi_call_acpi_prm_handler(efi_status_t (__efiapi *handler_addr)(efi_physical_addr_t,
+								void *),
+			  efi_physical_addr_t param_buffer_addr,
+			  void *context);
+
 /*
  * efi_runtime_service() function identifiers.
  * "NONE" is used by efi_recover_from_page_fault() to check if the page
@@ -1250,6 +1256,7 @@ enum efi_rts_ids {
 	EFI_RESET_SYSTEM,
 	EFI_UPDATE_CAPSULE,
 	EFI_QUERY_CAPSULE_CAPS,
+	EFI_ACPI_PRM_HANDLER,
 };
 
 union efi_rts_args;
