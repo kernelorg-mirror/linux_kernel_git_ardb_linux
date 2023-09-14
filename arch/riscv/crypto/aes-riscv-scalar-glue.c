2d63128532f67e3cfad7713da72edc5de718538e
Ard Biesheuvel <ardb@kernel.org>
2023-09-14 09:23:48 +0000

    crypto: riscv/aes - Provide scalar implementation based on Zkn extension
    The generic AES implementation that will be used if no architecture
    specific one is available relies on lookup tables that are relatively
    large with respect to the typical L1 D-cache size, which not only
    affects performance, it may also result in timing variances that
    make it susceptible to known-plaintext attacks on the encryption key.
    
    So we tend to avoid the generic code if we can, typically by using a
    driver that makes use of special AES instructions which incorporate all
    or most of the operations involved in performing a single round of AES.
    
    The Zkn RISC-V for RV32 extension provides another interesting take on
    this: it defines instructions operating on scalar registers that can
    replace the individual table lookups, eliminating the timing variances
    due to caching.  Those tables carry 32-bit quantities, making them a
    natural fit for a 32-bit architecture. And given the use of scalars, we
    don't have to rely in in-kernel SIMD, which is a bonus.
    
    For RV64, the instructions are more similar to SIMD AES instructions
    that other architectures implement, except for the fact that they output
    only half of the AES state, and therefore need to be issued as pairs.
    
    Note that this implementation does not use the RV64 key schedule
    generation instructions [yet]: for simplicity and symmetry with RV32
    (which has no special instructions for this purpose), the existing AES
    library code is used, which we need to carry in any case given that the
    Zkn extension is optional.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

---
+++ arch/riscv/crypto/aes-riscv-scalar-glue.c
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Scalar AES core transform using RISC-V Zkned extensionst
+ *
+ * Copyright (C) 2023 Google, LLC.
+ * Author: Ard Biesheuvel <ardb@kernel.org>
+ */
+
+#include <crypto/aes.h>
+#include <crypto/algapi.h>
+#include <crypto/internal/skcipher.h>
+#include <linux/module.h>
+
+asmlinkage void __aes_riscv_scalar_encrypt(u32 *rk, int rounds, u8 *out, const u8 *in);
+asmlinkage void __aes_riscv_scalar_decrypt(u32 *rk, int rounds, u8 *out, const u8 *in);
+
+static int aes_riscv_set_key(struct crypto_lskcipher *tfm, const u8 *in_key,
+			     unsigned int key_len)
+{
+	struct crypto_aes_ctx *ctx = crypto_lskcipher_ctx(tfm);
+
+	return aes_expandkey(ctx, in_key, key_len);
+}
+
+static int aes_riscv_encrypt(struct crypto_lskcipher *tfm, const u8 *src,
+			     u8 *dst, unsigned int nbytes, u8 *iv, bool final)
+{
+	struct crypto_aes_ctx *ctx = crypto_lskcipher_ctx(tfm);
+	int r = 6 + ctx->key_length / 4;
+
+	if (final && (nbytes % AES_BLOCK_SIZE))
+		return -EINVAL;
+
+	while (nbytes >= AES_BLOCK_SIZE) {
+		__aes_riscv_scalar_encrypt(ctx->key_enc, r, dst, src);
+
+		dst += AES_BLOCK_SIZE;
+		src += AES_BLOCK_SIZE;
+		nbytes -= AES_BLOCK_SIZE;
+	}
+	return nbytes;
+}
+
+static int aes_riscv_decrypt(struct crypto_lskcipher *tfm, const u8 *src,
+			     u8 *dst, unsigned int nbytes, u8 *iv, bool final)
+{
+	struct crypto_aes_ctx *ctx = crypto_lskcipher_ctx(tfm);
+	int r = 6 + ctx->key_length / 4;
+
+	if (final && (nbytes % AES_BLOCK_SIZE))
+		return -EINVAL;
+
+	while (nbytes >= AES_BLOCK_SIZE) {
+		__aes_riscv_scalar_decrypt(ctx->key_dec, r, dst, src);
+
+		dst += AES_BLOCK_SIZE;
+		src += AES_BLOCK_SIZE;
+		nbytes -= AES_BLOCK_SIZE;
+	}
+	return nbytes;
+}
+
+static struct lskcipher_alg aes_alg = {
+	.co.base.cra_name		= "aes",
+	.co.base.cra_driver_name	= "aes-riscv-scalar",
+	.co.base.cra_priority		= 200,
+	.co.base.cra_blocksize		= AES_BLOCK_SIZE,
+	.co.base.cra_ctxsize		= sizeof(struct crypto_aes_ctx),
+	.co.base.cra_module		= THIS_MODULE,
+	.co.min_keysize			= AES_MIN_KEY_SIZE,
+	.co.max_keysize			= AES_MAX_KEY_SIZE,
+
+	.setkey				= aes_riscv_set_key,
+	.encrypt			= aes_riscv_encrypt,
+	.decrypt			= aes_riscv_decrypt
+};
+
+static int __init riscv_aes_init(void)
+{
+	if (!riscv_isa_extension_available(NULL, ZKNE) ||
+	    !riscv_isa_extension_available(NULL, ZKND))
+		return -ENODEV;
+	return crypto_register_lskcipher(&aes_alg);
+}
+
+static void __exit riscv_aes_fini(void)
+{
+	crypto_unregister_lskcipher(&aes_alg);
+}
+
+module_init(riscv_aes_init);
+module_exit(riscv_aes_fini);
+
+MODULE_DESCRIPTION("Scalar AES lskcipher using RISC-V Zkned extension");
+MODULE_AUTHOR("Ard Biesheuvel <ardb@kernel.org>");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS_CRYPTO("aes");
