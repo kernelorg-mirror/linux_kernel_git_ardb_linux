1295be6d1a5cb6a30132ee8b01c79018794d0c3e
Ard Biesheuvel <ardb@kernel.org>
2024-09-23 16:54:07 +0200

    x86/tools: Drop x86_64 support from 'relocs' tool
    The relocs tool is no longer used on vmlinux, which is the only 64-bit
    ELF executable that it used to operate on in the 64-bit build. (It is
    still used for parts of the decompressor)
    
    So drop the 64-bit handling - it is dead code now.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/x86/tools/relocs.h
+++ arch/x86/tools/relocs.h
@@ -30,10 +30,7 @@ enum symtype {
 	S_NSYMTYPES
 };
 
-void process_32(FILE *fp, int use_real_mode, int as_text,
-		int show_absolute_syms, int show_absolute_relocs,
-		int show_reloc_info);
-void process_64(FILE *fp, int use_real_mode, int as_text,
-		int show_absolute_syms, int show_absolute_relocs,
-		int show_reloc_info);
+void process(FILE *fp, int use_real_mode, int as_text,
+	     int show_absolute_syms, int show_absolute_relocs,
+	     int show_reloc_info);
 #endif /* RELOCS_H */
