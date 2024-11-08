fae9daac3ee546fd029cf07f5257f76616ac922c
Ard Biesheuvel <ardb@kernel.org>
2024-11-08 19:38:45 +0100

    arm64/mm: Reduce PA space to 48 bits when LPA2 is not enabled
    Currently, LPA2 kernel support implies support for up to 52 bits of
    physical addressing, and this is reflected in global definitions such as
    PHYS_MASK_SHIFT and MAX_PHYSMEM_BITS.
    
    This is potentially problematic, given that LPA2 hardware support is
    modeled as a CPU feature which can be overridden, and with LPA2 hardware
    support turned off, attempting to map physical regions with address bits
    [51:48] set (which may exist on LPA2 capable systems booting with
    arm64.nolva) will result in corrupted mappings with a truncated output
    address and bogus shareability attributes.
    
    This means that the accepted physical address range in the mapping
    routines should be at most 48 bits wide when LPA2 support is configured
    but not enabled at runtime.
    
    Fixes: 352b0395b505 ("arm64: Enable 52-bit virtual addressing for 4k and 16k granule configs")
    Cc: <stable@vger.kernel.org>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm64/include/asm/sparsemem.h
+++ arch/arm64/include/asm/sparsemem.h
@@ -5,7 +5,9 @@
 #ifndef __ASM_SPARSEMEM_H
 #define __ASM_SPARSEMEM_H
 
-#define MAX_PHYSMEM_BITS	CONFIG_ARM64_PA_BITS
+#include <asm/pgtable-prot.h>
+
+#define MAX_PHYSMEM_BITS	PHYS_MASK_SHIFT
 
 /*
  * Section size must be at least 512MB for 64K base
