cc7dde411462cfac99a28c79aa2c6f463f1478ad
Ard Biesheuvel <ardb@kernel.org>
2025-05-03 13:12:24 +0200

    arm64/boot: Move init_pgdir[] and init_idmap_pgdir[] into __pi_ namespace
    init_pgdir[] is only referenced from the startup code, but lives after
    BSS in the linker map. Before tightening the rules about accessing BSS
    from startup code, move init_pgdir[] into the __pi_ namespace, so it
    does not need to be exported explicitly.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/arm64/kernel/image-vars.h
+++ arch/arm64/kernel/image-vars.h
@@ -54,10 +54,6 @@ PROVIDE(__pi_is_midr_in_range_list	= is_midr_in_range_list);
 PROVIDE(__pi__ctype			= _ctype);
 PROVIDE(__pi_memstart_offset_seed	= memstart_offset_seed);
 
-PROVIDE(__pi_init_idmap_pg_dir		= init_idmap_pg_dir);
-PROVIDE(__pi_init_idmap_pg_end		= init_idmap_pg_end);
-PROVIDE(__pi_init_pg_dir		= init_pg_dir);
-PROVIDE(__pi_init_pg_end		= init_pg_end);
 PROVIDE(__pi_swapper_pg_dir		= swapper_pg_dir);
 
 PROVIDE(__pi__text			= _text);
