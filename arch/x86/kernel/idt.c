ebd89b06f08f7a7f7ed0dd9b66c42cb959634b35
Ard Biesheuvel <ardb@kernel.org>
2025-12-29 14:29:07 +0100

    x86/idt: Move idt_table to __ro_after_init section
    Currently, idt_table is allocated as page-aligned .bss, and remapped
    read-only after init. If idt_table is located in a part of BSS that is
    covered by a huge page mapping, this may break it up into 4k page
    mappings, defeating some of the effort done at boot to map the kernel
    image using huge pages, for improved TLB efficiency.
    
    Mark this allocation as __ro_after_init instead, so it will be made
    read-only automatically after boot, without breaking up large page
    mappings.
    
    This also fixes a latent bug on i386, where the size of idt_table is
    less than a page, and so remapping it read-only could potentially affect
    other read-write variables too, if those are not page-aligned as well.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/x86/kernel/idt.c
+++ arch/x86/kernel/idt.c
@@ -173,7 +173,7 @@ static const __initconst struct idt_data apic_idts[] = {
 };
 
 /* Must be page-aligned because the real IDT is used in the cpu entry area */
-static gate_desc idt_table[IDT_ENTRIES] __page_aligned_bss;
+static gate_desc idt_table[IDT_ENTRIES] __aligned(PAGE_SIZE) __ro_after_init;
 
 static struct desc_ptr idt_descr __ro_after_init = {
 	.size		= IDT_TABLE_SIZE - 1,
@@ -310,9 +310,6 @@ void __init idt_setup_apic_and_irq_gates(void)
 	/* Map IDT into CPU entry area and reload it. */
 	idt_map_in_cea();
 	load_idt(&idt_descr);
-
-	/* Make the IDT table read only */
-	set_memory_ro((unsigned long)&idt_table, 1);
 
 	idt_setup_done = true;
 }
