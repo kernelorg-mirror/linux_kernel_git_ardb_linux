f8ace4920829b4140aa685aa80257b860b380e50
Ard Biesheuvel <ardb@kernel.org>
2026-03-15 20:00:58 +0100

    x86/efi: Only merge EFI memory map entries on 32-bit systems
    Commit
    
      202f9d0a4180 ("x86, efi: Merge contiguous memory regions of the same type and attribute")
    
    introduced a pass over the EFI memory map, ensuring that contiguous
    regions of the same type and attribute are coalesced into a single
    entry. This was needed because relative references may exist between
    those regions, and so the virtual remapping needs to preserve the
    relative placement of these regions. This virtual remapping was based on
    ioremap() at the time, which does not guarantee that adjacent physical
    addresses are mapped adjacently in the virtual space.
    
    Commit
    
      d2f7cbe7b26a ("x86/efi: Runtime services virtual mapping")
    
    introduced a new strategy for virtually remapping the EFI runtime
    services, which is now the only remaining one, and commit
    
      a5caa209ba9c ("x86/efi: Fix boot crash by mapping EFI memmap entries bottom-up at runtime, instead of top-down")
    
    tweaked the logic to ensure that the relative offset of adjacent regions
    of any type is preserved on 64-bit systems, by reversing the order in
    which the EFI memory map is traversed when choosing the virtual
    placement.
    
    This means that merging regions is no longer needed on 64-bit, given
    that the relative placement of adjacent regions is guaranteed to be
    preserved in the virtual space. So make this hack 32-bit only.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/x86/platform/efi/efi_32.c
+++ arch/x86/platform/efi/efi_32.c
@@ -152,3 +152,34 @@ void arch_efi_call_virt_teardown(void)
 	firmware_restrict_branch_speculation_end();
 	efi_fpu_end();
 }
+
+/* Merge contiguous regions of the same type and attribute */
+void __init efi_merge_regions(void)
+{
+	efi_memory_desc_t *md, *prev_md = NULL;
+
+	for_each_efi_memory_desc(md) {
+		u64 prev_size;
+
+		if (!prev_md) {
+			prev_md = md;
+			continue;
+		}
+
+		if (prev_md->type != md->type ||
+		    prev_md->attribute != md->attribute) {
+			prev_md = md;
+			continue;
+		}
+
+		prev_size = prev_md->num_pages << EFI_PAGE_SHIFT;
+
+		if (md->phys_addr == (prev_md->phys_addr + prev_size)) {
+			prev_md->num_pages += md->num_pages;
+			md->type = EFI_RESERVED_TYPE;
+			md->attribute = 0;
+			continue;
+		}
+		prev_md = md;
+	}
+}
