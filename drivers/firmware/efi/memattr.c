423fd29648410d598c4b6aa2afdd6d6174311d76
Ard Biesheuvel <ardb@kernel.org>
2026-03-26 14:27:00 +0100

    efi: Use efi_mem_reserve() to reserve the memory attribute table
    Use efi_mem_reserve() rather than memblock_reserve() to reserve the EFI
    memory attributes table. This is needed on x86, where memblock_reserve()
    is not sufficient for assets that may be placed in EFI boot services
    data memory.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- drivers/firmware/efi/memattr.c
+++ drivers/firmware/efi/memattr.c
@@ -57,7 +57,7 @@ void __init efi_memattr_init(void)
 	}
 
 	tbl_size = sizeof(*tbl) + tbl->num_entries * tbl->desc_size;
-	memblock_reserve(efi_mem_attr_table, tbl_size);
+	efi_mem_reserve(efi_mem_attr_table, tbl_size);
 	set_bit(EFI_MEM_ATTR, &efi.flags);
 
 unmap:
